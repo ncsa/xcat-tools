87267f7069f3dff20edb80cfeaeb5b2bae0e6b09
Andy Loftus <aloftus@gmail.com>
2020-05-22 00:17:59 -0500

    backup-node_configs.sh ; fixes #14
    New cron_script "backup-node_configs.sh"
    + Deprecates cron_script/puppet_pull_client_certs.sh
    + Deprecates cron_script/kerberos_pull_keytabs.sh
    + Backs up each source (from internal list) as a tgz
      in /install/files/compute/NODE
    + List of backup sources:
      - puppet ssl dir
      - puppet config file
      - kerberos keytab file
    
    New postscript "restore-node_configs.sh"
    + Deprecates kerberos_keytab
    + Untar each tgz file from /install/files/compute/NODE
      to it's original location on the node (see tar -P)
    
    Update postscript "puppet_configure"
    + Expects puppet ssl certs are restored by "restore-node_configs.sh"
    + Support for puppet config restored by "restore-node_configs.sh"
      - If config file is <1 hour old, assume it is a custom restore
        and do NOT set puppet server
      - xcat setting "site.puppet_master" is used only on first deploy of
        a new node
    + Depends on "restore-node_configs" will be run prior

---
+++ cron_scripts/backup-node_configs.sh
@@ -0,0 +1,180 @@
+#!/bin/bash
+
+###
+# Import libs
+###
+BASE=/root/xcat-tools
+LIB=$BASE/libs
+imports=( logging build_nodelist )
+for f in "${imports[@]}"; do
+    srcfn="${LIB}/${f}.sh"
+    [[ -f "$srcfn" ]] || {
+        echo "Failed to find lib file '$srcfn'"
+        exit 1
+    }
+    source "$srcfn"
+done
+
+
+###
+# GLOBAL VARIABLES
+###
+
+XCATBIN=/opt/xcat/bin
+XDSH=$XCATBIN/xdsh
+NODELS=$XCATBIN/nodels
+PRG=$(basename $0)
+BKUP_BASE=/install/files/compute
+NOW=$(date +%s)
+REFRESH_DAYS=7
+
+BACKUP_SOURCES=( \
+    /etc/puppetlabs/puppet/ssl \
+    /etc/puppetlabs/puppet/puppet.conf \
+    /etc/krb5.keytab \
+)
+
+
+###
+# FUNCTIONS
+###
+
+mk_tgz_name() {
+    [[ $DEBUG -eq $YES ]] && set -x
+    echo "$1" | tr -c 'a-zA-Z0-9.-' '_' | sed -e 's/^_//' -e 's/_$//'
+}
+
+
+is_stale() {
+    [[ $DEBUG -eq $YES ]] && set -x
+    local _fn="$1"
+    local _mtime=0
+    [[ -e "$_fn" ]] && _mtime=$(date -r "$_fn" +%s)
+    [[ $_mtime -le $MIN_BKUP_DATE ]]
+}
+
+
+bkup_node_data() {
+    [[ $DEBUG -eq $YES ]] && set -x
+    local _node="$1"
+    local _src="$2"
+    local _tgt="$3"
+    local _rv=0
+    if client_has_path "$_node" "$_src" ; then
+        log "Backup '$_src' -> '$_tgt'"
+        ssh "$_node" "tar czP $_src" >"$_tgt"
+        _rv=$?
+    else
+        log "Source '$_src' does not exist on node '$_node'. Skipping."
+    fi
+    return $_rv
+}
+
+client_has_path() {
+    # Check if client has ssl certs to be backed up
+    local node="$1"
+    local path="$2"
+    xdsh "$node" -z "stat $path" &>/dev/null
+}
+
+
+usage() {
+  cat <<ENDOFLINE
+
+$PRG
+    Backup puppet certs & config file on xcat master
+    so they can be redeployed during node rebuild.
+Usage:
+    $PRG [-hfvd] [nodenames | noderange]
+    where:
+        nodenames must be a space separated list of valid nodenames
+        or
+        noderange must be a valid noderange expression understood by nodels
+    If neither of nodenames or noderange are specified, $PRG will operate on all (known) nodes.
+
+OPTIONS:
+  -d         debug
+  -f         forcefully pull client certs (default action will skip backups
+             for nodes that have local files already.
+  -h         print help message and exit
+  -r         refresh rate, make a new backup every X days (default=7)
+  -v         verbose
+
+ENDOFLINE
+}
+
+
+###
+#  MAIN CONTENTS
+###
+
+# CMDLINE OPTIONS
+FORCE=$NO
+VERBOSE=$NO
+DEBUG=$NO
+while getopts ":dfhr:v" opt; do
+    case $opt in
+    d)  DEBUG=$YES
+        ;;
+    f)  FORCE=$YES
+        ;;
+    h)  usage
+        exit 0
+        ;;
+    r)  REFRESH_DAYS=$OPTARG
+        ;;
+    v)  VERBOSE=$YES
+        ;;
+    \?)
+        echo "Invalid option: -$OPTARG" >&2
+        exit 1
+        ;;
+    :)
+        echo "Option -$OPTARG requires an argument." >&2
+        exit 1
+        ;;
+  esac
+done
+shift $((OPTIND-1))
+let "REFRESH_SECS = 86400 * $REFRESH_DAYS"
+let "MIN_BKUP_DATE = $NOW - $REFRESH_SECS"
+[[ $FORCE -eq $YES ]] && MIN_BKUP_DATE=$NOW
+
+# ENABLE TRACING IF DEBUG REQUESTED
+[[ $DEBUG -eq $YES ]] && set -x
+
+if [[ $# -lt 1 ]] ; then
+  nodelist=( $( $NODELS all) )
+else
+  nodelist=( $( build_nodelist "$@" ) )
+fi
+
+BACKUP_TARGETS=()
+for src in "${BACKUP_SOURCES[@]}"; do
+    BACKUP_TARGETS+=( $(mk_tgz_name "$src" ) )
+done
+
+#for i in "${!BACKUP_SOURCES[@]}"; do
+#  echo $i
+#  echo "${BACKUP_SOURCES[$i]}"
+#  echo "${BACKUP_TARGETS[$i]}"
+#done
+#exit 12
+
+for n in "${nodelist[@]}" ; do
+  log "Node: '$n'"
+  node_bkup_dir="$BKUP_BASE/$n"
+  mkdir -p "$node_bkup_dir"
+  for i in "${!BACKUP_SOURCES[@]}"; do
+      src="${BACKUP_SOURCES[$i]}"
+      tgt_fn="${BACKUP_TARGETS[$i]}"
+      bkup_tgt="$node_bkup_dir/${tgt_fn}.tgz"
+      if is_stale "$bkup_tgt" ; then
+          bkup_node_data "$n" "$src" "$bkup_tgt" \
+          || warn "Failure detected during backup of '$src' from node '$n'"
+      else
+          log "OK '$src'"
+          continue
+      fi
+  done
+done
