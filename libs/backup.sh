3a06a9ac263898e59d070651048c68c82a4c175f
Andy Loftus <aloftus@gmail.com>
2020-06-23 11:25:23 -0500

    Use /var/backups/xcat

--- libs/backup.sh
+++ libs/backup.sh
@@ -2,8 +2,8 @@
 # so that /backup remains empty for a future boot.
 # When a dir is a mount point, it has a different device number than its parent
 MOUNTPOINT='/backup'
-MOUNTPOINT_ALTERNATIVE='/backup_local'
-if [[ $( stat -fc%t:%T "$MOUNTPOINT" ) != $( stat -fc%t:%T "$MOUNTPOINT/.." ) ]] ; then
+MOUNTPOINT_ALTERNATIVE='/var/backups/xcat'
+if [[ $( stat -fc%t:%T "$MOUNTPOINT" 2>/dev/null ) != $( stat -fc%t:%T "$MOUNTPOINT/.." ) ]] ; then
     BACKUPDIR="$MOUNTPOINT"
 else
     BACKUPDIR="$MOUNTPOINT_ALTERNATIVE"
