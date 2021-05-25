b398e9ceabf5f642cb266d6085b59857bb1aa387
Andy Loftus <aloftus@gmail.com>
2021-05-25 12:42:20 +0000

    add lldef
    like "ls -l" for lsdef, long listing of all objects of the given type

--- bashrc.d/xcat.sh
+++ bashrc.d/xcat.sh
@@ -1,14 +1,24 @@
-# override tabdump to align columns
+# ALIASES
+
+## override tabdump to align columns
 alias td=___INSTALL_DIR___/admin_scripts/tabdump.sh
 
-# override tabdump to align columns and filter empty cols
+## override tabdump to align columns and filter empty cols
 alias tdf='___INSTALL_DIR___/admin_scripts/tabdump.sh -f'
 
-# List only tables that have actual data in them
+## List only tables that have actual data in them
 alias tabls=___INSTALL_DIR___/admin_scripts/tabls.sh
 
-# short alias for rebuild_xcat_node
+## short alias for rebuild_xcat_node
 alias rebuild=___INSTALL_DIR___/admin_scripts/rebuild_xcat_node.sh
+
+# FUNCTIONS
+
+## lldef - long listing of all objects of the given type
+## like "ls -l" for lsdef
+lldef() {
+  lsdef -t "$1" | awk '{printf("%s,", $1)}' | xargs lsdef -t "$1" -l -o
+}
 
 # add xcat-tools to path
 [[ ":$PATH:" != *":___INSTALL_DIR___/admin_scripts:"* ]] \
