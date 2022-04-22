c52220969213a132dd242775249e1f51132674d7
Andy Loftus <aloftus@gmail.com>
2022-04-22 17:57:43 +0000

    add puppetserver, timezone options
    fail if no puppetserver found
    
    (fix #31)
    Add sshbetweennodes=NOGROUPS
    
    (fix #32)
    Add xcatsslciphers
    Add xcatsslversion

--- admin_scripts/01_setup_site.sh
+++ admin_scripts/01_setup_site.sh
@@ -21,7 +21,7 @@ for f in "${imports[@]}"; do
 done
 
 
-check_required_parameters() {
+assert_required_parameters_are_set() {
   [[ $DEBUG -eq $YES ]] && set -x
   local _req_params=( MASTERIP DOMAIN )
   for v in "${_req_params[@]}"; do
@@ -29,14 +29,20 @@ check_required_parameters() {
   done
 }
 
-get_pup_server() {
+
+assert_puppet_server_is_set() {
   [[ $DEBUG -eq $YES ]] && set -x
-  local _pupcmd=$(which puppet 2>/dev/null )
-  local _pupserver=puppet
-  [[ -n "$_pupcmd" ]] && {
-    _pupserver=$( $_pupcmd config print server --section=agent )
-  }
-  echo $_pupserver
+  if [[ -z "${PUPPETSERVER}" ]] ; then
+    # no puppet server given on cmdline, try to get it from the local server
+    local _pupcmd=$(which puppet 2>/dev/null )
+    local _pupserver=''
+    [[ -n "$_pupcmd" ]] && {
+      _pupserver=$( $_pupcmd config print server --section=agent )
+    }
+    [[ -z "${_pupserver}" ]] \
+    && croak "Cant determine puppet server. Run again with --puppetserver option.'"
+    PUPPETSERVER="$_pupserver"
+  fi
 }
 
 
@@ -51,7 +57,6 @@ END {
   print ns
 }' \
     /etc/resolv.conf )
-  local _puppetmaster=$( get_pup_server )
   cat <<ENDSTANZA
 clustersite:
   objtype=site
@@ -61,7 +66,11 @@ clustersite:
   forwarders=$_forwarders
   master=$MASTERIP
   nameservers=$_forwarders
-  puppetmaster=$_puppetmaster
+  puppetmaster=$PUPPETSERVER
+  sshbetweennodes=NOGROUPS
+  timezone="$TIMEZONE"
+  xcatsslciphers="HIGH:!3DES"
+  xcatsslversion=TLSv12
 ENDSTANZA
 }
 
@@ -76,13 +85,16 @@ Usage:
   $PRG [OPTIONS] --masterip <IPADDR> --domain <LOCAL.XCAT.DOMAIN>
 
 PARAMETERS:
-  --masterip   ip of xcat master node on the management network
-               (where nodes will pxe boot)
-  --domain     DNS domain name to be used on the management network
+  --domain    DNS domain name to be used on the management network
+  --masterip  IP of xcat master node on the management network
+              (where nodes will pxe boot)
 
 OPTIONS:
   -h    print help message and exit
   -n    dry-run, show what would be done, but don't make any changes
+  --puppetserver Hostname of the puppetserver to use for managed nodes.
+                 (Defaults to the puppetserver used on by xcat.)
+  --timezone  Defaults to "America/Chicago"
 
 ENDHERE
 }
@@ -90,6 +102,7 @@ ENDHERE
 DEBUG=$NO
 DRYRUN=$NO
 ENDWHILE=0
+TIMEZONE="America/Chicago"
 while [[ $# -gt 0 ]] && [[ $ENDWHILE -eq 0 ]] ; do
   case $1 in
     -h)
@@ -108,13 +121,20 @@ while [[ $# -gt 0 ]] && [[ $ENDWHILE -eq 0 ]] ; do
     --masterip)
         MASTERIP=$2;
         shift;;
+    --puppetserver)
+        PUPPETSERVER=$2;
+        shift;;
+    --timezone)
+        TIMEZONE=$2;
+        shift;;
     --) ENDWHILE=1;;
      *) ENDWHILE=1; break;;
   esac
   shift
 done
 
-check_required_parameters
+assert_required_parameters_are_set
+assert_puppet_server_is_set
 
 [[ $DEBUG -eq $YES ]] && set -x
 
